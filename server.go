2b21acca9176291306b1d7b5914cd87f74dc4540
Gearnode <friminb@gmail.com>
2018-05-13 15:48:55 +0200

    Add TLS connection

--- server.go
+++ server.go
@@ -1,33 +1,44 @@
 package judge
 
 import (
+	"crypto/tls"
 	"fmt"
-	"github.com/gearnode/judge/api"
 	"github.com/gearnode/judge/orn"
+	"github.com/gearnode/judge/svc"
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 	"log"
 	"net"
 )
 
-type Server struct{}
+// Server represent a server instance. This struct store the
+// server configuration.
+type Server struct {
+	Port int
+	Addr string
+	Cert *tls.Certificate
+}
 
-func Start(addr string, port int) error {
-	lis, err := net.Listen("tcp", fmt.Sprintf("%s:%d", addr, port))
+// Start a new server
+func (s *Server) Start() error {
+	lis, err := net.Listen("tcp", fmt.Sprintf("%s:%d", s.Addr, s.Port))
 	if err != nil {
 		return err
 	}
 
-	s := Server{}
-	opts := []grpc.ServerOption{}
+	creds := credentials.NewServerTLSFromCert(s.Cert)
+	opts := []grpc.ServerOption{grpc.Creds(creds)}
+
 	grpcServer := grpc.NewServer(opts...)
-	api.RegisterJudgeServer(grpcServer, &s)
+	svc.RegisterJudgeServer(grpcServer, s)
 
 	return grpcServer.Serve(lis)
 }
 
-func (s *Server) Authorize(ctx context.Context, in *api.AuthorizeRequest) (*api.AuthorizeResponse, error) {
+// Authorize todo
+func (s *Server) Authorize(ctx context.Context, in *svc.AuthorizeRequest) (*svc.AuthorizeResponse, error) {
 	log.Printf("Receive Authorize Request for execute %s on %s", in.Action, in.Orn)
 	ok, err := Authorize([]Policy{}, orn.ORN{}, "")
-	return &api.AuthorizeResponse{Authorize: ok}, err
+	return &svc.AuthorizeResponse{Authorize: ok}, err
 }
