252a31f2e2c44edc05373328e0bd0efe29a89e7f
Gearnode <friminb@gmail.com>
2018-06-16 19:13:06 +0200

    Add memory store

---
+++ pkg/storage/memory/memory_store.go
@@ -0,0 +1,101 @@
+/*
+Copyright 2018 The Judge Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+// Package memorystore contains an memory implementation of
+// the Judge storage interface.
+package memorystore // import "github.com/gearnode/judge/pkg/storage/memory"
+
+import (
+	"errors"
+
+	"github.com/gearnode/judge/pkg/storage"
+)
+
+// MemoryTable foo
+type MemoryTable map[string]MemoryRow
+
+// MemoryRow foo
+type MemoryRow map[string]interface{}
+
+// MemoryStore is a simple in-memory storage using the DB interface.
+type MemoryStore struct {
+	data MemoryTable
+
+	storage.DB
+}
+
+// NewMemoryStore create a new memory store
+func NewMemoryStore() *MemoryStore {
+	return &MemoryStore{data: MemoryTable{"settings": MemoryRow{}}}
+}
+
+// Data return the raw internal memory state.
+func (store *MemoryStore) Data() MemoryTable {
+	return store.data
+}
+
+// DescribeAll return all element in the given table
+func (store *MemoryStore) DescribeAll(table string) ([]interface{}, error) {
+	partition := store.data[table]
+	data := make([]interface{}, len(partition))
+
+	if partition == nil {
+		partition = MemoryRow{}
+	}
+
+	i := 0
+	for _, v := range partition {
+		data[i] = v
+		i++
+	}
+
+	return data, nil
+}
+
+// Describe return the elememt with the given ID in the given table
+func (store *MemoryStore) Describe(table string, id string) (interface{}, error) {
+	partition := store.data[table]
+
+	if partition == nil {
+		partition = MemoryRow{}
+	}
+
+	elem := partition[id]
+
+	if elem == nil {
+		return nil, errors.New("record not exist")
+	}
+
+	return elem, nil
+}
+
+// Put upsert record in the given table
+func (store *MemoryStore) Put(table string, id string, object interface{}) error {
+	partition := store.data[table]
+
+	if partition == nil {
+		partition = MemoryRow{}
+	}
+
+	partition[id] = object
+	store.data[table] = partition
+	return nil
+}
+
+// Delete remove an element in the given table
+func (store *MemoryStore) Delete(table string, id string) error {
+	return nil
+}
