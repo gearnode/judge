1641da7e92df032caac04920924aba8ee74ee6b0
Bryan FRIMIN <friminb@gmail.com>
2018-11-17 23:19:28 +0100

    Pkg/apiserver/add update policy endpoint (#26)
    * Add update policy api endpoint
    
    * Add override of default auto generate ORN support
    
    This allow user to generate policy object offline.

--- pkg/apiserver/server.go
+++ pkg/apiserver/server.go
@@ -105,11 +105,20 @@ func (s *Server) CreatePolicy(ctx context.Context, in *v1alpha1.CreatePolicyRequ
 	log.Info("Receive CreatePolicy Request to execute")
 
 	res := v1alpha1.Policy{}
+
 	pol, err := policy.NewPolicy(in.GetPolicy().GetName(), in.GetPolicy().GetDescription())
 	if err != nil {
 		return &v1alpha1.Policy{}, status.Error(codes.InvalidArgument, err.Error())
 	}
 
+	if in.GetOrn() != "" {
+		var id orn.ORN
+		err := orn.Unmarshal(in.GetOrn(), &id)
+		if err != nil {
+			return &v1alpha1.Policy{}, status.Error(codes.InvalidArgument, err.Error())
+		}
+		pol.ORN = id
+	}
 	res.Orn = orn.Marshal(&pol.ORN)
 	res.Name = pol.Name
 	res.Description = pol.Description
@@ -153,7 +162,56 @@ func (s *Server) CreatePolicy(ctx context.Context, in *v1alpha1.CreatePolicyRequ
 // UpdatePolicy implement judge.api.v1alpha1.Judge.UpdatePolicy
 func (s *Server) UpdatePolicy(ctx context.Context, in *v1alpha1.UpdatePolicyRequest) (*v1alpha1.Policy, error) {
 	log.Info("Receive UpdatePolicy Resquest to execute")
-	return &v1alpha1.Policy{}, nil
+
+	res := v1alpha1.Policy{}
+	pol, err := policy.NewPolicy(in.GetPolicy().GetName(), in.GetPolicy().GetDescription())
+	if err != nil {
+		return &v1alpha1.Policy{}, status.Error(codes.InvalidArgument, err.Error())
+	}
+
+	if in.GetPolicy().GetOrn() != "" {
+		var id orn.ORN
+		err := orn.Unmarshal(in.GetPolicy().GetOrn(), &id)
+		if err != nil {
+			return &v1alpha1.Policy{}, status.Error(codes.InvalidArgument, err.Error())
+		}
+		pol.ORN = id
+	}
+
+	res.Orn = orn.Marshal(&pol.ORN)
+	res.Name = pol.Name
+	res.Description = pol.Description
+
+	for _, v := range in.GetPolicy().GetDocument().GetStatements() {
+		stm, err := policy.NewStatement(v.GetEffect(), v.GetActions(), v.GetResources())
+		if err != nil {
+			return &v1alpha1.Policy{}, status.Error(codes.InvalidArgument, err.Error())
+		}
+		pol.Document.Statement = append(pol.Document.Statement, *stm)
+	}
+
+	store.Put("policies", res.Orn, *pol)
+
+	statements := make([]*v1alpha1.Statement, len(pol.Document.Statement))
+	for i, v := range pol.Document.Statement {
+
+		stm := v1alpha1.Statement{Effect: v.Effect, Actions: v.Action}
+
+		for _, v := range v.Resource {
+			r := resource.Marshal(&v)
+			stm.Resources = append(stm.Resources, r)
+		}
+		statements[i] = &stm
+	}
+
+	doc := v1alpha1.Document{
+		Version:    pol.Document.Version,
+		Statements: statements,
+	}
+
+	res.Document = &doc
+
+	return &res, nil
 }
 
 // DeletePolicy implement judge.api.v1alpha1.Judge.DeletePolicy
